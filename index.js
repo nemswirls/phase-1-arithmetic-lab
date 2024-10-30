6b35cf40a9d559de97c71e9ee63ee616c6c42e5b
Nemanja Stankovic <nemanjas0823@hotmail.com>
2024-10-29 23:41:09 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 =2;
+let num2 =62/num1;
+let num3 = 14
+let num4= 10
+const multiply = num1 *num2;
+const random= Math.floor(Math.random() * 100) + 1;
+const mod= num3 - num4
+const max= Math.max(0,20)
